36a2f02a84006e33ac3361a17e9d51fdd3094272
Tor Kristian Dahle <tk@torkristian.tech>
2018-12-10 23:03:38 +0100

    Updates

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from flask import Flask, request, render_template
 import serial
+import requests
 import time
 app = Flask(__name__)
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 @app.route("/", methods=['GET', 'POST'])
 def test():
-    #data = ser.readline().decode('utf-8')
-    data = "test"
+    led = "STANDBY - Default mode"
+ #   led = ser.readline().decode('utf-8')
     if request.method == 'POST':
         if request.form["submit"] == "blink":
-            data = "blink"
-            ser.write(b'1')
+            led = "blink"
+  #          ser.write(b'1')
         elif request.form["submit"] == "solid":
-            data = "solid"
-            ser.write(b'2')
+            led = "solid"
+   #         ser.write(b'2')
+        elif request.form["submit"] == "Up":
+            led = "Moving arm - UP"
+    #        ser.write(b'3')
+        elif request.form["submit"] == "Down":
+            led = "Moving arm - DOWN"
+     #       ser.write(b'4')
+        elif request.form["submit"] == "Left":
+            led = "Moving arm - LEFT"
+      #      ser.write(b'5')
+        elif request.form["submit"] == "Right":
+            led = "Moving arm - RIGHT"
+       #     ser.write(b'6')
 
 
-
-    return render_template('base.html', values = data)
+    return render_template('base.html', values = led)
 
 if __name__=='__main__':
     app.run(debug = True)
